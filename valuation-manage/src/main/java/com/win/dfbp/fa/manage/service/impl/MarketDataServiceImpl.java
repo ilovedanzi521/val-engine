e4acb775e1c4a2d95d16d4bc34c7c5f183142209
zoujian <zoujian@ysstech.com>
2019-10-09 15:43:56 +0800

    [add-新增功能] 功能描述: 新建目录 影响范围: StoryID :

---
+++ valuation-manage/src/main/java/com/win/dfbp/fa/manage/service/impl/MarketDataServiceImpl.java
@@ -0,0 +1,30 @@
+/****************************************************
+ * 创建人: @author zoujian    
+ * 创建时间: 2019-10-9/15:39
+ * 项目名称: dfbp-fa-engine
+ * 文件名称: MarketDataServiceImpl.java
+ * 文件描述: @Description: 行情数据service
+ *
+ * All rights Reserved, Designed By 投资交易团队
+ * @Copyright:2016-2019
+ *
+ ********************************************************/
+package com.win.dfbp.fa.manage.service.impl;
+
+import com.win.dfbp.fa.manage.service.MarketDataService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 包名称：com.win.dfbp.fa.manage.service.impl
+ * 类名称：MarketDataServiceImpl
+ * 类描述：行情数据service
+ * 创建人：@author zoujian
+ * 创建时间：2019-10-9/15:39
+ */
+@Service
+public class MarketDataServiceImpl implements MarketDataService {
+    @Override
+    public void updateValMarket() {
+
+    }
+}
---
+++ valuation-manage/src/main/java/com/win/dfbp/fa/manage/util/ReadFileUtil.java
@@ -0,0 +1,106 @@
+/****************************************************
+ * 创建人: @author zoujian    
+ * 创建时间: 2019-10-9/15:31
+ * 项目名称: dfbp-fa-engine
+ * 文件名称: ReadFileUtil.java
+ * 文件描述: @Description: 
+ *
+ * All rights Reserved, Designed By 投资交易团队
+ * @Copyright:2016-2019
+ *
+ ********************************************************/
+package com.win.dfbp.fa.manage.util;
+
+import com.win.dfbp.fa.manage.entity.ValMarket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 包名称：com.win.dfbp.fa.manage.util
+ * 类名称：ReadFileUtil
+ * 类描述：
+ * 创建人：@author zoujian
+ * 创建时间：2019-10-9/15:31
+ */
+public class ReadFileUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadFileUtil.class);
+
+    /**
+     * 读取中证txt行情数据
+     * @Title: readTxt
+     * @param filePath
+     * @return: java.util.List<com.win.dfbp.fa.manage.entity.ValMarket>
+     * @throws
+     * @author: zoujian
+     * @Date:  2019-10-9/15:40
+     */
+    public static  List<ValMarket> readTxt(String filePath) {
+        // 行情数据数组
+        List<ValMarket> marketList = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+            if (file.isFile() && file.exists()) {
+                InputStreamReader isr = new InputStreamReader(new FileInputStream(file), "utf-8");
+                BufferedReader br = new BufferedReader(isr);
+                String lineTxt = null;
+                boolean isBegin = false;
+                String beginStr = "==========";
+                while ((lineTxt = br.readLine()) != null) {
+                    ValMarket valMarket =  ValMarket.getValMarket();
+                    // 从==========开始，正式读取有效数据
+                    if(isBegin){
+                        String[] arrStrings = lineTxt.split("\\|");
+                        if(arrStrings.length < 10){
+                            LOGGER.info("数据有误！");
+                            continue;
+                        }
+                        valMarket.setDataSource("zw");
+                        // 日期
+                        valMarket.setValuationDate(arrStrings[0]);
+                        // 全价
+                        valMarket.setFullPrice(arrStrings[4]);
+                        // 净价
+                        valMarket.setNetPrice(arrStrings[8]);
+                        // 上交所证券代码
+                        if(arrStrings[1] != null && !"".equals(arrStrings[1].trim())){
+                            ValMarket valMarket1 =  ValMarket.getValMarket();
+                            BeanUtils.copyProperties(valMarket,valMarket1);
+                            valMarket1.setSecurityCode(arrStrings[1]);
+                            marketList.add(valMarket1);
+                        }
+                        // 深交所证券代码
+                        if(arrStrings[2] != null && !"".equals(arrStrings[2].trim())){
+                            ValMarket valMarket1 =  ValMarket.getValMarket();
+                            BeanUtils.copyProperties(valMarket,valMarket1);
+                            valMarket1.setSecurityCode(arrStrings[2]);
+                            marketList.add(valMarket1);
+                        }
+                        // 银行间债券代码
+                        if(arrStrings[3] != null && !"".equals(arrStrings[3].trim())){
+                            ValMarket valMarket1 =  ValMarket.getValMarket();
+                            BeanUtils.copyProperties(valMarket,valMarket1);
+                            valMarket1.setSecurityCode(arrStrings[3]);
+                            marketList.add(valMarket1);
+                        }
+                    }
+                    if(beginStr.equals(lineTxt)){
+                        isBegin = true;
+                    }
+                }
+
+            }
+        }catch (Exception e) {
+            LOGGER.info("文件读取错误！");
+        }
+        return marketList;
+    }
+}
