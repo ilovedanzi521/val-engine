87f27710cec1157841ee80f0c8df64dd02b20442
huhe <huhe@ysstech.com>
2019-10-17 09:54:57 +0800

    [add-新增功能] 功能描述:添加估值查询的方法接口 影响范围: StoryID :

---
+++ valuation-manage/src/main/java/com/win/dfbp/val/manage/dao/ValCalculationManageMapper.java
@@ -0,0 +1,60 @@
+/****************************************************
+ * 创建人：     @author huhe    
+ * 创建时间: 2019年10月16日/上午11:37:19
+ * 项目名称：  valuation-manage
+ * 文件名称: ValCalculationManageMapper.java
+ * 文件描述: @Description: TODO
+ *
+ * All rights Reserved, Designed By 投资交易团队
+ * @Copyright:2016-2019
+ * 
+ ********************************************************/
+
+package com.win.dfbp.val.manage.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.win.dfbp.val.manage.vo.query.ValCalculationItemQueryVO;
+import com.win.dfbp.val.manage.vo.query.ValCalculationManageQueryVO;
+import com.win.dfbp.val.manage.vo.respone.ValCalculationItemRepVO;
+import com.win.dfbp.val.manage.vo.respone.ValCalculationManageRepVO;
+
+/**   
+ * 包名称： com.win.dfbp.val.manage.dao 
+ * 类名称：ValCalculationManageMapper 
+ * 类描述：估值方法管理mapper
+ * 创建人：@author huhe 
+ * 创建时间：2019年10月16日/上午11:37:19
+ *     
+ */
+@Mapper
+public interface ValCalculationManageMapper {
+	
+	/**
+	 * 
+	 * @Title: getValCalculationManages
+	 * @Description: 查询估值方法管理列表
+	 * @param reqVO
+	 * @return   
+	 * @return: List<ValCalculationManageRepVO>   
+	 * @throws
+	 * @author: huhe 
+	 * @Date:  2019年10月17日/上午9:06:49
+	 */
+	List<ValCalculationManageRepVO> getValCalculationManages(ValCalculationManageQueryVO reqVO);
+	
+	/**
+	 * 
+	 * @Title: updataCalculationManage
+	 * @Description: 更新管理方法
+	 * @param reqVO   
+	 * @return: void   
+	 * @throws
+	 * @author: huhe 
+	 * @Date:  2019年10月17日/上午9:39:48
+	 */
+	void updataCalculationManage(ValCalculationManageQueryVO reqVO);
+	
+}
